967a51e05835eb8cc01a85f5de034e2a362ebe86
Matt Haigh <matthew.haigh@opower.com>
2015-06-22 15:50:39 -0400

    Naive bayes homework solution

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -16,18 +16,41 @@ from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
+print "Calling preprocess()"
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
+print "Preprocess is done."
 
 
 #########################################################
 ### your code goes here ###
 
+print len(features_train)
+print len(features_test)
+print len(labels_train)
+print len(labels_test)
+
+from sklearn.metrics import accuracy_score
+from sklearn.naive_bayes import GaussianNB
+
+classifier = GaussianNB()
+
+print "Fitting"
+pre_fit_time = time()
+classifier.fit(features_train, labels_train)
+print "training time:", round(time()-pre_fit_time, 3), "s"
+
+print "Predicting"
+pre_predict_time = time()
+prediction = classifier.predict(features_test)
+print "training time:", round(time()-pre_predict_time, 3), "s"
+
+print "accuracy:"
+print accuracy_score(labels_test, prediction)
 
 #########################################################
 
